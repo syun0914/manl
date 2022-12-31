33fe5ca4580a64570784823f5475edcc0b35223b
syun0914 <jisoohyun0914@gmail.com>
2022-12-31 15:16:37 +0900

    image url edit

--- routers/kakao_v1.py
+++ routers/kakao_v1.py
@@ -41,7 +41,7 @@ async def skill(
     REFRESH = tem.QReply('🌀 새로고침', 'block', '🌀 새로고침', bi)
 
     if bn == '급식' or bn == '생일 급식':
-        image_url = 'https://rawcdn.githack.com/syun0914/manl_thumbnail/main/winter_2022/2022_winter_1_compressed.png'
+        image_url = 'https://rawcdn.githack.com/syun0914/manl_thumbnail/f19f16a42ec0b6a0ac512119d2b90a3c51d0674b/winter_2022/2022_winter_1_compressed.png'
         if not await permission(user_key):
             return tem.basicCard(image_url, '사용 불가', WEAK)
         d = await meal(j.loads(params['date'])['date'], params['meal_time'])
@@ -51,7 +51,7 @@ async def skill(
         )
 
     elif bn == '시간표':
-        image_url = 'https://rawcdn.githack.com/syun0914/manl_thumbnail/main/winter_2022/2022_winter_2_compressed.png'
+        image_url = 'https://rawcdn.githack.com/syun0914/manl_thumbnail/f19f16a42ec0b6a0ac512119d2b90a3c51d0674b/winter_2022/2022_winter_2_compressed.png'
         if not await permission(user_key):
             return tem.basicCard(image_url, '사용 불가', WEAK)
         d = await timetable('3-1', params['day'])
